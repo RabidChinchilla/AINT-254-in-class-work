5ae12721e1f393575e71a7d63360079d09d9627c
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2018-10-08 14:04:41 +0100

    added last weeks vectors work into the folder

---
+++ Vectors/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+    private Transform m_cubeTransform;
+    private Rigidbody m_cubeRigidBody;
+
+    [SerializeField]
+    [Range(0.1f, 200)]
+    private float m_speed = 5.0f;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_cubeTransform = transform;
+        m_cubeRigidBody = m_cubeTransform.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //m_cubeTransform.position = m_cubeTransform.position + (m_cubeTransform.forward * m_speed * Time.deltaTime);
+        m_cubeRigidBody.velocity = m_cubeTransform.forward * m_speed * Time.deltaTime;
+
+        //m_cubeRigidBody.AddForce(m_cubeTransform.forward * m_speed * Time.deltaTime);
+	}
+}
